ed144be87694b3af3513cffc6c4389479a7a79f2
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-02-15 19:43:23 -0300

    add language pt-br

--- robots/text.js
+++ robots/text.js
@@ -10,7 +10,10 @@ async function robot(content) {
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2?timeout=300')
-        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaResponse = await wikipediaAlgorithm.pipe({
+            "lang": "pt",
+            "articleName": content.searchTerm
+        })
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content

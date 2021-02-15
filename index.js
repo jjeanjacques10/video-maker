9c97ed70b9d3407502f71dad292411d17e31ff6f
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-02-15 19:02:06 -0300

    feat: add text robot

---
+++ index.js
@@ -0,0 +1,30 @@
+const readline = require('readline-sync')
+const robots = {
+    userInput: require('./robots/user-input.js'),
+    text: require('./robots/text.js')
+}
+
+async function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+    //robots.userInput(content)
+    await robots.text(content)
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes)
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
---
+++ robots/user-input.js
@@ -0,0 +1,14 @@
+/* content.searchTerm = askAndReturnSearchTerm()
+content.prefix = askAndReturnPrefix()
+
+function askAndReturnSearchTerm() {
+    return readline.question('Type a Wikipedia search term: ')
+}
+
+function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes)
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
+} */

095db56b75570d1f8526e94274bdbc3c74d52496
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-02-19 15:06:13 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const robots = {
     input: require('./robots/user-input.js'),
-    text: require('./robots/text.js')
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start() {
-    robots.input()
-    await robots.text()
+/*     robots.input()
+    await robots.text() */
+    await robots.image()
 
     const content = robots.state.load()
     console.dir(content, { depht: null })
